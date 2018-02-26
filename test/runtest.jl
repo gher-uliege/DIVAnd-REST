b9e24a0271490aa5bad41d29fb89735efc411e5b
Alexander Barth <barth.alexander@gmail.com>
2018-02-26 16:01:20 +0100

    bathymetry test

--- test/runtest.jl
+++ test/runtest.jl
@@ -2,6 +2,17 @@ using Base.Test
 
 import HTTP
 import JSON
+import divand
+using NCDatasets
+
+
+# Version of the REST API
+const version = "v1"
+
+
+const bathdatasets = Dict{String,Tuple{String,Bool}}(
+   "GEBCO" => ("gebco_30sec_16.nc",true))
+
 
 """
    strbbox = encodebbox(bbox)
@@ -15,41 +26,41 @@ decodelist(strlist) = parse.(Float64,split(strlist,","))
 
 
 function savebathnc(filename,b,xy)
-  x,y = xy
-ds = Dataset("filename.nc","c")
-# Dimensions
+    x,y = xy
+    ds = Dataset(filename,"c")
+    # Dimensions
 
-ds.dim["lat"] = size(b,2)
-ds.dim["lon"] = size(b,1)
+    ds.dim["lat"] = size(b,2)
+    ds.dim["lon"] = size(b,1)
 
-# Declare variables
+    # Declare variables
 
-nclat = defVar(ds,"lat", Float64, ("lat",))
-nclat.attrib["long_name"] = "Latitude"
-nclat.attrib["standard_name"] = "latitude"
-nclat.attrib["units"] = "degrees_north"
+    nclat = defVar(ds,"lat", Float64, ("lat",))
+    nclat.attrib["long_name"] = "Latitude"
+    nclat.attrib["standard_name"] = "latitude"
+    nclat.attrib["units"] = "degrees_north"
 
-nclon = defVar(ds,"lon", Float64, ("lon",))
-nclon.attrib["long_name"] = "Longitude"
-nclon.attrib["standard_name"] = "longitude"
-nclon.attrib["units"] = "degrees_east"
+    nclon = defVar(ds,"lon", Float64, ("lon",))
+    nclon.attrib["long_name"] = "Longitude"
+    nclon.attrib["standard_name"] = "longitude"
+    nclon.attrib["units"] = "degrees_east"
 
-ncbat = defVar(ds,"bat", Float32, ("lon", "lat"))
-ncbat.attrib["long_name"] = "elevation above sea level"
-ncbat.attrib["standard_name"] = "height"
-ncbat.attrib["units"] = "meters"
+    ncbat = defVar(ds,"bat", Float32, ("lon", "lat"))
+    ncbat.attrib["long_name"] = "elevation above sea level"
+    ncbat.attrib["standard_name"] = "height"
+    ncbat.attrib["units"] = "meters"
 
-# Global attributes
+    # Global attributes
 
-#ds.attrib["title"] = "GEBCO"
+    #ds.attrib["title"] = "GEBCO"
 
-# Define variables
+    # Define variables
 
-nclat[:] = x
-nclon[:] = y
-ncbat[:] = b
+    nclat[:] = x
+    nclon[:] = y
+    ncbat[:] = b
 
-close(ds)
+    close(ds)
 end
 
 # minlon,minlat,maxlon,maxlat
@@ -59,51 +70,75 @@ bbox = [-180,-90,180,90]
 list = [1,2]
 @test decodelist(encodelist(list)) == list
 
-   
+
 
 
 router = HTTP.Router()
 
-function testfun(req::HTTP.Request)
-  params = HTTP.queryparams(HTTP.URI(req.target))
-  @show params
-  minlon,minlat,maxlon,maxlat = decodebbox(params["bbox"])
-  reslon,reslat = decodelist(params["resolution"])
-  
-  bathname = "gebco_30sec_16.nc"
-  isglobal = true
-  
-  xi,yi,bath = divand.load_bath(bathname,isglobal,minlon:reslon:maxlon,minlat:reslat:maxlat)
-  
-  @show minlon,minlat,maxlon,maxlat
-  @show reslon,reslat
-  @show xi
+function sendfile(code,filename)
+    f = open(filename)
+    data = read(f)
+    close(f)
 
-  #filename = tempname()
-  #savebathnc(filename,bath,(xi,yi))
-  #stream = HTTP.stream(open(filename))
-  HTTP.Response(200,"Hi")
-  #return HTTP.Response(200,stream)
-  #return HTTP.Stream(HTTP.Response(200),open("test.txt"))
+    return HTTP.Response(code,data)
+end
+
+function bathymetry(req::HTTP.Request)
+    params = HTTP.queryparams(HTTP.URI(req.target))
+    @show params
+    minlon,minlat,maxlon,maxlat = decodebbox(params["bbox"])
+    reslon,reslat = decodelist(params["resolution"])
+    dataset = params["dataset"]
+
+    
+    bathname,isglobal = bathdatasets[dataset]
+    
+    xi,yi,bath = divand.load_bath(bathname,isglobal,minlon:reslon:maxlon,minlat:reslat:maxlat)
+    
+    @show minlon,minlat,maxlon,maxlat
+    @show reslon,reslat
+    @show xi
+    filename = tempname()
+    #filename = "/tmp/tmp2.nc"
+    #if isfile(filename)
+    #    rm(filename)
+    #end
+    savebathnc(filename,bath,(xi,yi))
+
+    return sendfile(200,filename) 
+    #stream = HTTP.stream(open(filename))
+    #HTTP.Response(200,"Hi")
+    #return HTTP.Response(200,stream)
+    #return HTTP.Stream(HTTP.Response(200),open("test.txt"))
 end
 
 
-HTTP.register!(router, "GET", "/testfun",HTTP.HandlerFunction(testfun))
-	
+HTTP.register!(router, "GET", "/v1/bathymetry",HTTP.HandlerFunction(bathymetry))
+
 server = HTTP.Servers.Server(router)
-task = @async HTTP.serve(server, ip"127.0.0.1", 8000; verbose=false)
+task = @async HTTP.serve(server, ip"127.0.0.1", 8001; verbose=false)
 
 sleep(1.0)
 
 
-req = HTTP.request("GET","http://127.0.0.1:8000/testfun/"; query = Dict(
-   "bbox" => encodebbox([-10,30,50,45]),
-   "resolution" => encodebbox([1,1])
-   )
-   )
+req = HTTP.request("GET","http://127.0.0.1:8001/v1/bathymetry/"; query = Dict(
+    "bbox" => encodebbox([-10,30,50,45]),
+    "resolution" => encodelist([1,1]),
+    "dataset" => "GEBCO"
+))
+
+
+ncfile = tempname()
+open(ncfile,"w") do f
+    write(f,req.body)
+end
 
 
 # end server
 put!(server.in, HTTP.Servers.KILL)
 
-@test String(req.body) == "Hi"
+
+#@test String(req.body) == "Hi"
+
+@test "bat" in Dataset(ncfile)
+
