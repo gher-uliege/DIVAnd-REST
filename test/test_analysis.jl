804af58b42466ae4546ae8080a4082d583c4240a
Alexander Barth <barth.alexander@gmail.com>
2018-03-02 23:02:52 +0100

    clean-up

--- test/test_analysis.jl
+++ test/test_analysis.jl
@@ -16,15 +16,16 @@ URL2 = "http://127.0.0.1:8001" * Dict(resp.headers)["Location"]
 while true
     resp = HTTP.request("GET",URL2; redirect = false)
     headers = Dict(resp.headers)
-    @show URL2,headers,String(resp.body)
+    @show URL2
 
-    if haskey(headers,"Location")    
-        URL3 = "http://127.0.0.1:8001" * headers["Location"]        
+    if haskey(headers,"Location")
+        URL3 = "http://127.0.0.1:8001" * headers["Location"]
         resp = HTTP.request("GET",URL3)
         break
     else
-        sleep(1)
-        print("retry")
+        maxage,seconds = split(headers["Cache-Control"],"=")
+        println("retry in $(seconds) seconds")
+        sleep(parse(Float64,seconds))
     end
 end
 
