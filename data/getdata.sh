a400a920e3ee6b692cf9b714f2c399ea6e605500
Alexander Barth <barth.alexander@gmail.com>
2019-02-04 16:05:59 +0100

    update URL

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,17 +1,17 @@
 if ! [ -f gebco_30sec_4.nc ]; then
-    wget -O gebco_30sec_4.nc 'https://b2drop.eudat.eu/s/ACcxUEZZi6a4ziR/download'
+    wget -O gebco_30sec_4.nc 'https://dox.ulg.ac.be/index.php/s/RSwm4HPHImdZoQP/download'
 fi
 
 if ! [ -f gebco_30sec_16.nc ]; then
-    wget -O gebco_30sec_16.nc 'https://b2drop.eudat.eu/s/o0vinoQutAC7eb0/download'
+    wget -O gebco_30sec_16.nc 'https://dox.ulg.ac.be/index.php/s/U0pqyXhcQrXjEUX/download'
 fi
 
 if ! [ -f WOD-Salinity.nc ]; then
-    wget -O WOD-Salinity.nc 'http://b2drop.eudat.eu/s/UsF3RyU3xB1UM2o/download'
+    wget -O WOD-Salinity.nc 'https://dox.ulg.ac.be/index.php/s/PztJfSEnc8Cr3XN/download'
 fi
 
 if ! [ -f sample-file.nc ]; then
-    wget -O sample-file.nc 'https://b2drop.eudat.eu/s/Sk4zTDTi3tgmetR/download'
+    wget -O sample-file.nc 'https://dox.ulg.ac.be/index.php/s/FosTu5frviI8chW/download'
 fi
 
 
