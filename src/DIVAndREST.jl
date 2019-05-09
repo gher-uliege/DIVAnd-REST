b4b7fde7adc2eefc7af9a670bfa38bec40612431
Alexander Barth <barth.alexander@gmail.com>
2019-05-09 10:02:41 +0200

    point WOD-Salinity to correct file

--- src/DIVAndREST.jl
+++ src/DIVAndREST.jl
@@ -48,8 +48,8 @@ const bathdatasets = Dict{String,Tuple{String,Bool}}(
 
 
 const datalist = Dict{String,String}(
-    #"WOD-Salinity" => "data/WOD-Salinity.nc",
-    "WOD-Salinity" => "data/sample-file.nc",
+    "WOD-Salinity" => "data/WOD-Salinity.nc",
+    #"WOD-Salinity" => "data/sample-file.nc",
     "ODV-sample" => "data/ODV-sample.txt",
     "gebco_30sec_16" => "data/gebco_30sec_16.nc",
     "gebco_30sec_4" => "data/gebco_30sec_4.nc",
